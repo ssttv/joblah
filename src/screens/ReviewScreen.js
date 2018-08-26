fb32fc7b80c1d47cb5af0c518de67d199138f6f5
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-08-26 22:26:23 +0300

    feat: init navigation options for the review screen

--- src/screens/ReviewScreen.js
+++ src/screens/ReviewScreen.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import { View, Text } from "react-native";
 
 class ReviewScreen extends Component {
+  static navigationOptions = {
+    title: ReviewJobs
+  };
+
   render() {
     return (
       <View>
