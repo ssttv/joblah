383f3141b3e07b2c422b4b34b256d7ac5ac18956
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-08-27 23:12:11 +0300

    chore: add Android-specific functionality

--- src/screens/ReviewScreen.js
+++ src/screens/ReviewScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Platform } from "react-native";
 import { Button } from "react-native-elements";
 
 class ReviewScreen extends Component {
