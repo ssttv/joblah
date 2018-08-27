0d57adf2a4583874eb4202481a452593faffb575
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-08-27 23:24:05 +0300

    feat: put data into slides

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -2,9 +2,13 @@ import React, { Component } from "react";
 import { View, Text } from "react-native";
 import { Slides } from "../components/Slides";
 
+const SLIDE_DATA = [
+  { text: "Welcome to Joblah" },
+  { text: "Enter your location, and swipe away" }
+];
 class WelcomeScreen extends Component {
   render() {
-    return <Slides />;
+    return <Slides data={SLIDE_DATA} />;
   }
 }
 
