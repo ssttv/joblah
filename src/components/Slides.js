b021f02f8bb1ac0553cb8cc6951d3710bb1467c9
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-08-28 03:01:52 +0300

    feat: put styling onto slides

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 import { View, ScrollView, StyleSheet, Text } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  slide: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  slideText: {
+    fontSize: 30
+  }
+});
 
 class Slides extends Component {
   renderSlides() {
     return this.props.data.map(slide => {
       return (
-        <View key={slide.text}>
-          <Text>{slide.text}</Text>
+        <View key={slide.text} style={styles.slide}>
+          <Text style={styles.slideText}>{slide.text}</Text>
         </View>
       );
     });
