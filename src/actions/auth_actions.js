f9f387dfb51d806afcfe8179a454fe4e72c75b55
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-08-30 07:48:09 +0300

    feat: write up a token check dispatch function

--- src/actions/auth_actions.js
+++ src/actions/auth_actions.js
@@ -1,1 +1,12 @@
-import FACEBOOK_LOGIN_SUCCESS from "./types";
+import { AsyncStorage } from "react-native";
+
+import { FACEBOOK_LOGIN_SUCCESS } from "./types";
+
+export const facebookLogin = () => async dispatch => {
+  let token = await AsyncStorage.getItem("fb_token");
+  if token {
+
+  } else {
+
+  }
+}
