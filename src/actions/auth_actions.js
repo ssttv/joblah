394ecee16c0db0d40933a5ae6681f545c60c1e0e
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-08-30 08:06:07 +0300

    feat: build async auth action

--- src/actions/auth_actions.js
+++ src/actions/auth_actions.js
@@ -1,12 +1,22 @@
 import { AsyncStorage } from "react-native";
-
-import { FACEBOOK_LOGIN_SUCCESS } from "./types";
+import { FACEBOOK_LOGIN_SUCCESS, FACEBOOK_LOGIN_FAIL } from "./types";
+import { Facebook } from "expo";
 
 export const facebookLogin = () => async dispatch => {
   let token = await AsyncStorage.getItem("fb_token");
   if token {
-
+    dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token })
   } else {
-
+    doFacebookLogin(dispatch)
   }
+}
+
+const doFacebookLogin = async dispatch => {
+  let { type, token } = await Facebook.logInWithReadPermissionsAsync("316019915822148", { permissions: ["public_profile"] });
+  if (type === "cancel") {
+    return dispatch({ type: FACEBOOK_LOGIN_FAIL })
+  }
+
+  await AsyncStorage.setItem("fb_token", token)
+  dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token })
 }
