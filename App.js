b200653495fe375d82ee6cbd872b7f075e418ccc
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-09-01 23:54:29 +0300

    chore: correct padding to adjust for changes in code

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-    paddingTop: "20px"
+    paddingTop: "15px"
   }
 });
 
