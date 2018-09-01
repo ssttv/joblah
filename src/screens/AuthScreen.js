400bc8b0a3b087532acef12c7c2e4c5fbb36a06e
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-09-01 23:59:27 +0300

    feat: write code for the auth page lifecycle

--- src/screens/AuthScreen.js
+++ src/screens/AuthScreen.js
@@ -4,6 +4,10 @@ import { connect } from "react-redux";
 import * as actions from "../actions";
 
 class AuthScreen extends Component {
+  componentDidMount() {
+    this.props.facebookLogin();
+  }
+
   render() {
     return (
       <View>
