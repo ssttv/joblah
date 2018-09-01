d5cc45c81eff3aad5c44c8360542f562fc533486
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-09-01 23:55:56 +0300

    import actions into auth

--- src/screens/AuthScreen.js
+++ src/screens/AuthScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 import { connect } from "react-redux";
+import * as actions from "../actions";
 
 class AuthScreen extends Component {
   render() {
