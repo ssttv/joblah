5c2bf77cb061730886942267e55eb43cb03fefb9
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-09-02 23:56:44 +0300

    fix: set initial state for reducers

--- src/reducers/auth_reducer.js
+++ src/reducers/auth_reducer.js
@@ -1,8 +1,10 @@
 import { FACEBOOK_LOGIN_SUCCESS } from "../actions/types";
 
-export default function(state, action) {
+export default function(state = {}, action) {
   switch (action.type) {
     case FACEBOOK_LOGIN_SUCCESS:
       return { token: action.payload };
+    default:
+      return state;
   }
 }
