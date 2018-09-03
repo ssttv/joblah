dac451e765f5b0bcac8d7d5bca97d5c695e86b14
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-09-03 07:10:50 +0300

    chore: clean up some age cases

--- src/reducers/auth_reducer.js
+++ src/reducers/auth_reducer.js
@@ -4,6 +4,8 @@ export default function(state = {}, action) {
   switch (action.type) {
     case FACEBOOK_LOGIN_SUCCESS:
       return { token: action.payload };
+    case FACEBOOK_LOGIN_FAIL:
+      return { token: null };
     default:
       return state;
   }
