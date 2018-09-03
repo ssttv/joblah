4f421950c368343a47f77788d55add802c862caa
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-09-03 10:17:41 +0300

    chore: delete boilerplate in auth screen

--- src/screens/AuthScreen.js
+++ src/screens/AuthScreen.js
@@ -21,15 +21,7 @@ class AuthScreen extends Component {
   }
 
   render() {
-    return (
-      <View>
-        <Text>AuthScreen</Text>
-        <Text>AuthScreen</Text>
-        <Text>AuthScreen</Text>
-        <Text>AuthScreen</Text>
-        <Text>AuthScreen</Text>
-      </View>
-    );
+    return <View />;
   }
 }
 
