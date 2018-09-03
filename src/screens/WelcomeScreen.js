a673518cd5aad038d68cb3a047031434ff902d4e
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-09-03 10:32:53 +0300

    feat: use lodash to properly skip welcome screen

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,5 +1,7 @@
+import _ from "lodash";
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { AppLoading } from "expo";
+import { View, Text, AsyncStorage } from "react-native";
 import { Slides } from "../components/Slides";
 
 const SLIDE_DATA = [
@@ -8,11 +10,25 @@ const SLIDE_DATA = [
   { text: "Enter your location, and swipe away", color: "#03A9F4" }
 ];
 class WelcomeScreen extends Component {
+  state = {token: null};
+
+  async componentWillMount() {
+    let token = await AsyncStorage.getItem("fb_token");
+    if (token) {
+      this.props.navigation.navigate("map");
+    } else {
+      this.setState({ token: false });
+    }
+  }
+
   onSlidesComplete = () => {
     this.props.navigation.navigate("auth"); 
   }
   
   render() {
+    if (_.isNull(this.state.token)) {
+      return <AppLoading />;
+    }
     return <Slides data={SLIDE_DATA} onComplete={this.onSlidesComplete} />;
   }
 }
