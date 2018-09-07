acf5ed52391bf4d8163f2be305b2412b67b64d11
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-09-07 23:21:46 +0300

    feat: return a spinner if map is not loaded

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -4,6 +4,7 @@ import { MapView } from "expo";
 
 class MapScreen extends Component {
   state = {
+    mapLoaded: false,
     region: {
       longitude: -122,
       latitude: 37,
@@ -12,6 +13,13 @@ class MapScreen extends Component {
     }
   }
   render() {
+    if {!this.state.mapLoaded} {
+      return(
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <ActivityIndicator size="large" />
+        </View>
+      )
+    }
     return (
       <View style={{ flex: 1 }}>
         <MapView region={this.state.region} style={{ flex: 1 }} />
