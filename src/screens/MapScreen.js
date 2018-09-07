cb83e10174c5c50ad4dcb05f723be324e85b4bfa
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-09-07 23:22:54 +0300

    chore: add missing imports

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
 import { MapView } from "expo";
 
 class MapScreen extends Component {
