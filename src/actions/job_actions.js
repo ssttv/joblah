6d685cc9326ca81ec70d18a7f693ea005c98a25d
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-09-11 20:59:35 +0300

    feat: get zipcode from an async request

--- src/actions/job_actions.js
+++ src/actions/job_actions.js
@@ -2,4 +2,6 @@ import axios from "axios";
 import FETCH_JOBS from "./types";
 import reverseGeocode from "latlng-to-zip";
 
-export const fetchJobs = region => async dispatch => {};
+export const fetchJobs = region => async dispatch => {
+  let zipcode = await reverseGeocode(region);
+};
