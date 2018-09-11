3f6e96137387a9796f57a5f40b733005efe3dde3
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-09-11 21:02:44 +0300

    chore: properly catch errors in async zipcode request

--- src/actions/job_actions.js
+++ src/actions/job_actions.js
@@ -3,5 +3,9 @@ import FETCH_JOBS from "./types";
 import reverseGeocode from "latlng-to-zip";
 
 export const fetchJobs = region => async dispatch => {
-  let zipcode = await reverseGeocode(region);
+  try {
+    let zipcode = await reverseGeocode(region);
+  } catch (e) {
+    console.error(e);
+  }
 };
