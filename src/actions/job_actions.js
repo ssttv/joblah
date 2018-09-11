c3704272ab83dd2aaee6f9e295afdf4281cc205f
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-09-11 21:13:54 +0300

    feat: hardcode job query params into the basic version

--- src/actions/job_actions.js
+++ src/actions/job_actions.js
@@ -2,6 +2,14 @@ import axios from "axios";
 import FETCH_JOBS from "./types";
 import reverseGeocode from "latlng-to-zip";
 
+const JOB_QUERY_PARAMS = {
+  publisher: "4201738803816157",
+  format: "json",
+  v: "2",
+  latlong: 1,
+  radius: 10,
+  q: "javascript"
+};
 export const fetchJobs = region => async dispatch => {
   try {
     let zipcode = await reverseGeocode(region);
