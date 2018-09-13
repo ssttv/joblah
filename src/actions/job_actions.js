6b870e82464040f7934d5ec8dd50f976555de61c
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-09-13 21:39:19 +0300

    fix: correct spelling for preview

--- src/actions/job_actions.js
+++ src/actions/job_actions.js
@@ -12,7 +12,7 @@ const JOB_QUERY_PARAMS = {
 };
 export const fetchJobs = region => async dispatch => {
   try {
-    let zipcode = await reverseGeocode(region);
+    let zip = await reverseGeocode(region);
   } catch (e) {
     console.error(e);
   }
